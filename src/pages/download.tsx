9031ec6719155cd78a6d9ccb964986222bc5dd5c
Abdullah Çat <abdullahcat@Abdullah-MacBook-Pro.local>
2025-03-07 15:05:53 +0300

    changes

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -7,23 +7,20 @@ const AppRedirect = () => {
     const userAgent = navigator.userAgent || navigator.vendor;
 
     if (/android/i.test(userAgent)) {
-      setStoreLink("https://play.google.com/store/apps/details?id=your.android.app.id");
+      setStoreLink("https://play.google.com/store/apps/details?id=com.standyroutes.standy");
     } else if (/iPad|iPhone|iPod/.test(userAgent)) {
-      setStoreLink("https://apps.apple.com/app/your-ios-app-id");
-    } else if (/Macintosh/i.test(userAgent) && navigator.maxTouchPoints && navigator.maxTouchPoints > 1) {
-      // This ensures we detect macOS devices that support touch (MacBooks with Apple Silicon)
-      setStoreLink("https://apps.apple.com/app/your-ios-app-id");
-    } else {
+      setStoreLink("https://apps.apple.com/tr/app/standy-%C3%B6%C4%9Frenci-i-ndirimleri/id6741170791");
+    }  else {
       setStoreLink(null);
     }
   }, []);
 
   return (
     <div> 
-       <div className="flex flex-col items-center justify-center min-h-screen">
+       <div className="flex flex-col bg-white items-center justify-center min-h-screen">
         <p className="text-lg font-semibold text-gray-700">Choose your app store:</p>
         {storeLink ? (
-          <a href={storeLink} className="mt-4 px-6 py-3 bg-blue-600 text-white rounded-lg">Go to Store</a>
+          <a href={storeLink} className="mt-4 px-6 py-3 bg-pink-600 text-white rounded-lg">Go to Store</a>
         ) : (
           <p className="text-gray-600 mt-4">App not available for this device.</p>
         )}
